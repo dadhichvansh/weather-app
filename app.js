464de03e8c6b6634adb588f8df4f9da9f9de8621
vansh-home <dadhichvansh46@gmail.com>
2025-01-26 18:26:28 +0530

    Update app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const cityName = document.querySelector(".weather_city");
+const dateTime = document.querySelector(".weather_datetime");
+const w_forecast = document.querySelector(".weather_forecast");
+const w_temperature = document.querySelector(".weather_temperature");
+const w_icon = document.querySelector(".weather_icon");
+const w_minTemp = document.querySelector(".weather_min");
+const w_maxTemp = document.querySelector(".weather_max");
